c83cee571609cca9bd12fe5344020aa88fcc4ed2
WalterWoshid <wotschel.valentin@googlemail.com>
2025-12-14 23:56:51 +0100

    lexer/tests/tags: add tests for HTML chunk tokenization

--- crates/phynix-lex/tests/tags.rs
+++ crates/phynix-lex/tests/tags.rs
@@ -1,7 +1,8 @@
 mod util;
 
 use crate::util::{assert_kinds_eq, kinds};
-use phynix_lex::TokenKind;
+use phynix_core::{LanguageKind, Strictness};
+use phynix_lex::{lex, TokenKind};
 
 #[test]
 fn echo_open_tag_is_tokenized() {
@@ -15,4 +16,23 @@ fn echo_open_tag_is_tokenized() {
             TokenKind::Eof,
         ],
     );
+}
+
+#[test]
+fn html_only_emits_single_html_chunk_to_eof() {
+    let src = "hello <b>world</b>\nno php tags here";
+    let k = kinds(src);
+
+    assert_kinds_eq(&k, &[TokenKind::HtmlChunk, TokenKind::Eof]);
+}
+
+#[test]
+fn html_only_chunk_span_covers_entire_input() {
+    let src = "abc\nxyz";
+    let out =
+        lex(src, LanguageKind::PhpCompat, Strictness::Lenient).expect("lex ok");
+
+    assert_eq!(out.tokens[0].kind, TokenKind::HtmlChunk);
+    assert_eq!(out.tokens[0].span.start, 0);
+    assert_eq!(out.tokens[0].span.end as usize, src.len());
 }
