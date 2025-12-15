961cedafbe7fbed7e6ab7c516e283b2675165fd3
WalterWoshid <wotschel.valentin@googlemail.com>
2025-12-15 01:11:02 +0100

    lexer/tests/operators: add tests for compound assignment operators

--- crates/phynix-lex/tests/operators.rs
+++ crates/phynix-lex/tests/operators.rs
@@ -76,3 +76,138 @@ fn or_or_operator_is_tokenized() {
         ],
     );
 }
+
+#[test]
+fn dot_eq_is_tokenized() {
+    let k = kinds_php_prefixed("$a .= 1;");
+    assert_kinds_eq(
+        &k,
+        &[
+            TokenKind::VarIdent,
+            TokenKind::DotEq,
+            TokenKind::Int,
+            TokenKind::Semicolon,
+            TokenKind::Eof,
+        ],
+    );
+}
+
+#[test]
+fn plus_eq_is_tokenized() {
+    let k = kinds_php_prefixed("$a += 1;");
+    assert_kinds_eq(
+        &k,
+        &[
+            TokenKind::VarIdent,
+            TokenKind::PlusEq,
+            TokenKind::Int,
+            TokenKind::Semicolon,
+            TokenKind::Eof,
+        ],
+    );
+}
+
+#[test]
+fn minus_eq_is_tokenized() {
+    let k = kinds_php_prefixed("$a -= 1;");
+    assert_kinds_eq(
+        &k,
+        &[
+            TokenKind::VarIdent,
+            TokenKind::MinusEq,
+            TokenKind::Int,
+            TokenKind::Semicolon,
+            TokenKind::Eof,
+        ],
+    );
+}
+
+#[test]
+fn mul_eq_is_tokenized() {
+    let k = kinds_php_prefixed("$a *= 2;");
+    assert_kinds_eq(
+        &k,
+        &[
+            TokenKind::VarIdent,
+            TokenKind::MulEq,
+            TokenKind::Int,
+            TokenKind::Semicolon,
+            TokenKind::Eof,
+        ],
+    );
+}
+
+#[test]
+fn div_eq_is_tokenized() {
+    let k = kinds_php_prefixed("$a /= 2;");
+    assert_kinds_eq(
+        &k,
+        &[
+            TokenKind::VarIdent,
+            TokenKind::DivEq,
+            TokenKind::Int,
+            TokenKind::Semicolon,
+            TokenKind::Eof,
+        ],
+    );
+}
+
+#[test]
+fn mod_eq_is_tokenized() {
+    let k = kinds_php_prefixed("$a %= 2;");
+    assert_kinds_eq(
+        &k,
+        &[
+            TokenKind::VarIdent,
+            TokenKind::ModEq,
+            TokenKind::Int,
+            TokenKind::Semicolon,
+            TokenKind::Eof,
+        ],
+    );
+}
+
+#[test]
+fn amp_eq_is_tokenized() {
+    let k = kinds_php_prefixed("$a &= $b;");
+    assert_kinds_eq(
+        &k,
+        &[
+            TokenKind::VarIdent,
+            TokenKind::AmpEq,
+            TokenKind::VarIdent,
+            TokenKind::Semicolon,
+            TokenKind::Eof,
+        ],
+    );
+}
+
+#[test]
+fn pipe_eq_is_tokenized() {
+    let k = kinds_php_prefixed("$a |= $b;");
+    assert_kinds_eq(
+        &k,
+        &[
+            TokenKind::VarIdent,
+            TokenKind::PipeEq,
+            TokenKind::VarIdent,
+            TokenKind::Semicolon,
+            TokenKind::Eof,
+        ],
+    );
+}
+
+#[test]
+fn caret_eq_is_tokenized() {
+    let k = kinds_php_prefixed("$a ^= $b;");
+    assert_kinds_eq(
+        &k,
+        &[
+            TokenKind::VarIdent,
+            TokenKind::CaretEq,
+            TokenKind::VarIdent,
+            TokenKind::Semicolon,
+            TokenKind::Eof,
+        ],
+    );
+}
