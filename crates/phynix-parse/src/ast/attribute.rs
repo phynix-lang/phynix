ad65f49147694fcc532be286828228f325352e20
WalterWoshid <wotschel.valentin@googlemail.com>
2025-12-25 15:18:34 +0100

    parser/stmt/attribute: handle correctly

--- crates/phynix-parse/src/ast/attribute.rs
+++ crates/phynix-parse/src/ast/attribute.rs
@@ -1,5 +1,26 @@
-use phynix_core::Span;
+use crate::ast::{Expr, Ident, QualifiedName};
+use phynix_core::{Span, Spanned};
 
 pub struct AttributeGroup {
+    pub attrs: Vec<Attribute>,
     pub span: Span,
+}
+
+impl Spanned for AttributeGroup {
+    fn span(&self) -> Span {
+        self.span
+    }
+}
+
+#[derive(Debug)]
+pub struct Attribute {
+    pub name: QualifiedName,
+    pub args: Vec<AttributeArg>,
+    pub span: Span,
+}
+
+#[derive(Debug)]
+pub enum AttributeArg {
+    Positional { expr: Expr, span: Span },
+    Named { name: Ident, expr: Expr, span: Span },
 }
--- crates/phynix-parse/src/parser/stmt/attribute.rs
+++ crates/phynix-parse/src/parser/stmt/attribute.rs
@@ -1,6 +1,6 @@
-use crate::ast::AttributeGroup;
+use crate::ast::{Attribute, AttributeArg, AttributeGroup, Ident};
 use crate::parser::Parser;
-use phynix_core::Span;
+use phynix_core::{Span, Spanned};
 use phynix_lex::TokenKind;
 
 impl<'src> Parser<'src> {
@@ -37,6 +37,7 @@ impl<'src> Parser<'src> {
 
         let attr_open_token = self.bump();
         let start_span = attr_open_token.span;
+        let mut attrs = Vec::new();
 
         loop {
             while self.at(TokenKind::Backslash) {
@@ -47,30 +48,120 @@ impl<'src> Parser<'src> {
                 break;
             }
 
-            if !self.at(TokenKind::Ident) {
+            let attr_start = self.current_span().start;
+
+            let Some(name) =
+                self.parse_qualified_name("expected attribute name")
+            else {
                 self.error_and_recover(
                     "expected attribute name",
                     &[TokenKind::Comma, TokenKind::RBracket],
                 );
-            } else {
-                let _name_token = self.bump();
-                loop {
-                    if self.at(TokenKind::Backslash) {
-                        self.bump();
-                        if self.at(TokenKind::Ident) {
-                            self.bump();
-                            continue;
-                        } else {
-                            self.error_here(
-                                "expected identifier after '\\' in attribute name",
+                if self.at(TokenKind::RBracket) {
+                    break;
+                }
+                if self.eat(TokenKind::Comma) {
+                    continue;
+                }
+                continue;
+            };
+
+            let mut args = Vec::new();
+
+            if self.eat(TokenKind::LParen) {
+                if !self.at(TokenKind::RParen) {
+                    loop {
+                        let arg_start = self.current_span().start;
+
+                        if (self.at(TokenKind::Ident)
+                            || self.is_ident_like_kw(self.kind()))
+                            && self.at_nth(1, TokenKind::Colon)
+                        {
+                            let name_tok = self.expect_ident(
+                                "expected identifier in named attribute argument",
                             );
-                            break;
+                            let _colon = self.bump();
+
+                            let Some(expr) = self.parse_expr() else {
+                                self.error_and_recover(
+                                    "expected expression after named-arg ':' in attribute",
+                                    &[TokenKind::Comma, TokenKind::RParen],
+                                );
+                                if self.eat(TokenKind::Comma) {
+                                    if self.at(TokenKind::RParen) {
+                                        break;
+                                    }
+                                    continue;
+                                }
+                                break;
+                            };
+
+                            let arg_end = expr.span().end;
+                            let name_tok = name_tok?;
+                            let name_ident = Ident {
+                                span: name_tok.span,
+                            };
+
+                            args.push(AttributeArg::Named {
+                                name: name_ident,
+                                expr,
+                                span: Span {
+                                    start: arg_start,
+                                    end: arg_end,
+                                },
+                            });
+                        } else {
+                            let Some(expr) = self.parse_expr() else {
+                                self.error_and_recover(
+                                    "expected expression in attribute args",
+                                    &[TokenKind::Comma, TokenKind::RParen],
+                                );
+                                if self.eat(TokenKind::Comma) {
+                                    if self.at(TokenKind::RParen) {
+                                        break;
+                                    }
+                                    continue;
+                                }
+                                break;
+                            };
+
+                            let arg_end = expr.span().end;
+                            args.push(AttributeArg::Positional {
+                                expr,
+                                span: Span {
+                                    start: arg_start,
+                                    end: arg_end,
+                                },
+                            });
                         }
-                    } else {
+
+                        if self.eat(TokenKind::Comma) {
+                            if self.at(TokenKind::RParen) {
+                                break;
+                            }
+                            continue;
+                        }
+
                         break;
                     }
                 }
+
+                let _ = self.expect(
+                    TokenKind::RParen,
+                    "expected ')' in attribute args",
+                );
             }
+
+            let attr_end =
+                self.prev_span().map(|s| s.end).unwrap_or(attr_start);
+            attrs.push(Attribute {
+                name,
+                args,
+                span: Span {
+                    start: attr_start,
+                    end: attr_end,
+                },
+            });
 
             if self.eat(TokenKind::LParen) {
                 if !self.at(TokenKind::RParen) {
@@ -147,6 +238,9 @@ impl<'src> Parser<'src> {
             end: end_span.end,
         };
 
-        Some(AttributeGroup { span: full_span })
+        Some(AttributeGroup {
+            attrs,
+            span: full_span,
+        })
     }
 }
