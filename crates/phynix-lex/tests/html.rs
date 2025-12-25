3be87ac53d8518a93a1b037357f2030f2992678a
WalterWoshid <wotschel.valentin@googlemail.com>
2025-12-25 13:31:50 +0100

    lexer/tests/html: add test

---
+++ crates/phynix-lex/tests/html.rs
@@ -0,0 +1,24 @@
+use crate::util::{assert_kinds_eq_including_trivia, kinds};
+use phynix_lex::TokenKind;
+
+mod util;
+
+#[test]
+fn html_chunk_before_open_tag_emits_htmlchunk_and_then_opens_php() {
+    let src = "hello <?php $x ?>";
+
+    let k = kinds(src);
+
+    assert_kinds_eq_including_trivia(
+        &k,
+        &[
+            TokenKind::HtmlChunk,
+            TokenKind::PhpOpen,
+            TokenKind::WS,
+            TokenKind::VarIdent,
+            TokenKind::WS,
+            TokenKind::PhpClose,
+            TokenKind::Eof,
+        ],
+    );
+}
